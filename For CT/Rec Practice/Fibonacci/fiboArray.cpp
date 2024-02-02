6bf474b5145a24a2cb9c161fea3bf70a1822e1cc
Khan Israk Ahmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2024-02-02 14:59:26 +0600

    Practice Problem for CT

---
+++ For CT/Rec Practice/Fibonacci/fiboArray.cpp
@@ -0,0 +1,27 @@
+//        ****************  Author :  Khan Israk Ahmed  *************************\
+//        ****************  Date:    17-10-2023     *************************\
+
+/*Find Fibonacci Numbers using Dynamic ProgrammingFind Fibonacci Numbers using Dynamic Programming*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int genFibonacci(int n)
+{
+    int fibo[n + 2]; // array to store fibonacci values
+    // 0th and 1st number of the series are 0 and 1
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo[n];
+}
+int main()
+{
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    cout << n << " th Fibonacci Terms: " << genFibonacci(n) << endl;
+}
