c558dc09933423a5bb8f9411700c77ba849ff6d1
Khan Israk Ahmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2024-02-15 01:11:48 +0600

    added for Final

--- Dynamic Programming/Fibonacci_Bottom-UP_approach__Tabulation-Method.cpp
+++ Dynamic Programming/Fibonacci_Bottom-UP_approach__Tabulation-Method.cpp
@@ -1,27 +1,19 @@
+//        ****************  Author :  Khan Israk Ahmed  *************************\
+//        ****************  Date:    30-11-2023     *************************\
 
-//        ****************  Author :  Tashin.Parvez  ****************
-//        ************* United International University *************
-//        ****************  Updated:    27/10/23     ****************
 
 #include <bits/stdc++.h>
-#define CRACKED return 0;
-#define nl endl // NewLine
-#define null NULL
-#define sp " " // space
-#define setDec(x) fixed << setprecision(x)
-#define len(s) s.length()
-#define deb(n) cout << "Tashin   " << #n << " = " << n << endl; // debug code
-
-#define int long long
-const int N = 1e5 + 2;
 using namespace std;
 
-int dpTable[N];
+const int N = 1e5 + 2;
 
-int32_t main()
+long long dpTable[N];
+
+int main()
 {
     int n;
     cin >> n;
+
     dpTable[1] = 0;
     dpTable[2] = 1;
 
@@ -30,7 +22,7 @@ int32_t main()
         dpTable[i] = dpTable[i - 1] + dpTable[i - 2];
     }
 
-    cout << "\n Fibonacci series " << n << "th element is : " << dpTable[n] << nl << nl;
+    cout << "\nFibonacci series " << n << "th element is: " << dpTable[n] << "\n\n";
 
-    CRACKED;
+    return 0;
 }
