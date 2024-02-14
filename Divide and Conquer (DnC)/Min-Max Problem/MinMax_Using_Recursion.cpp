13b6639c5ea08cea0dd93b1b7ae8f9aedd7b1cca
Khan Israk Ahmed <130216292+KhanIsrakAhmed@users.noreply.github.com>
2024-02-15 01:11:40 +0600

    added for Final

--- Divide and Conquer (DnC)/Min-Max Problem/MinMax_Using_Recursion.cpp
+++ Divide and Conquer (DnC)/Min-Max Problem/MinMax_Using_Recursion.cpp
@@ -1,17 +1,8 @@
+//        ****************  Author :  Khan Israk Ahmed  *************************\
+//        ****************  Date:    30-11-2023     *************************\
 
-//        ****************  Author :  Tashin.Parvez  ****************
-//        ************* United International University *************
-//        ****************  Updated:    27/10/23     ****************
 
 #include <bits/stdc++.h>
-#define CRACKED return 0;
-#define nl endl // NewLine
-#define null NULL
-#define sp " " // space
-#define setDec(x) fixed << setprecision(x)
-#define len(s) s.length()
-#define deb(n) cout << "Tashin   " << #n << " = " << n << endl; // debug code
-
 using namespace std;
 
 class Result
@@ -23,7 +14,7 @@ public:
 
 Result minMax(int arr[], int index, int lastIndex)
 {
-    if (index == lastIndex) // checking is it last index or not
+    if (index == lastIndex)
     {
         Result result;
         result.minimum = arr[index];
@@ -32,8 +23,7 @@ Result minMax(int arr[], int index, int lastIndex)
     }
     else
     {
-        Result friend_ = minMax(arr, index + 1, lastIndex); /// Calling a Friend
-
+        Result friend_ = minMax(arr, index + 1, lastIndex);
         Result result;
         result.minimum = min(arr[index], friend_.minimum);
         result.maximum = max(arr[index], friend_.maximum);
@@ -41,30 +31,12 @@ Result minMax(int arr[], int index, int lastIndex)
     }
 }
 
-int32_t main()
+int main()
 {
-    int n;
-
-    // //--> for user inputs
-    // cin >> n;
-    // int arr[n];
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cin >> arr[i];
-    // }
-
-    n = 8;
+    int n = 8;
     int arr[n] = {-2, -5, 6, -2, -3, 1, 5, -6};
-
-    // n = 10;
-    // int arr[n] = {2, 5, 8, 3, 6, 9, 1, 4, 7, 0};
-
     Result result = minMax(arr, 0, n - 1);
-
-    cout << nl;
-    cout << "Minimum value " << result.minimum << nl;
-    cout << "Maximum value " << result.maximum << nl;
-    cout << nl;
-
-    CRACKED;
+    cout << "Minimum value: " << result.minimum << "\n";
+    cout << "Maximum value: " << result.maximum << "\n";
+    return 0;
 }
--- Divide and Conquer (DnC)/Min-Max Problem/MinMax_algo_DnC.cpp
+++ Divide and Conquer (DnC)/Min-Max Problem/MinMax_algo_DnC.cpp
@@ -1,18 +1,10 @@
+//        ****************  Author :  Khan Israk Ahmed  *************************\
+//        ****************  Date:    30-11-2023     *************************\
 
-//        ****************  Author :  Tashin.Parvez  ****************
-//        ************* United International University *************
-//        ****************  Updated:    27/10/23     ****************
 
 #include <bits/stdc++.h>
-#define CRACKED return 0;
-#define nl endl // NewLine
-#define null NULL
-#define sp " " // space
-#define setDec(x) fixed << setprecision(x)
-#define len(s) s.length()
-#define deb(n) cout << "Tashin   " << #n << " = " << n << endl; // debug code
-
 using namespace std;
+
 class Result
 {
 public:
@@ -20,11 +12,11 @@ public:
     int maximum;
 };
 
-Result minMax(int arr[], int s, int e) /// DnC
+Result minMax(int arr[], int s, int e)
 {
-    if (e - s == 1 || s == e) /// when array have 2 elements or one element
+    if (e - s == 1 || s == e)
     {
-        Result result; /// we can use pair also
+        Result result;
         result.minimum = min(arr[s], arr[e]);
         result.maximum = max(arr[s], arr[e]);
         return result;
@@ -32,42 +24,21 @@ Result minMax(int arr[], int s, int e) /// DnC
     else
     {
         int mid = s + (e - s) / 2;
-
-        Result leftSide = minMax(arr, s, mid);      ///  calling Friend 1
-        Result rightSide = minMax(arr, mid + 1, e); ///  calling Friend 2 
-
+        Result leftSide = minMax(arr, s, mid);
+        Result rightSide = minMax(arr, mid + 1, e);
         Result result;
         result.maximum = max(leftSide.maximum, rightSide.maximum);
         result.minimum = min(leftSide.minimum, rightSide.minimum);
-
         return result;
     }
 }
 
-int32_t main()
+int main()
 {
-    int n;
-
-    // ///---> for user inputs
-    // cin >> n;
-    // int arr[n];
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cin >> arr[i];
-    // }
-
-    n = 8;
-    int arr[n] = {-2, -5, 6, -2, -3, 1, 5, -6}; /// TestCase 1
-
-    // n = 10;
-    // int arr[n] = {2, 5, 8, 3, 6, 9, 1, 4, 7, 0}; /// TestCase 2
-
+    int n = 8;
+    int arr[n] = {-2, -5, 6, -2, -3, 1, 5, -6};
     Result result = minMax(arr, 0, n - 1);
-
-    cout << nl;
-    cout << "Minimum value " << result.minimum << nl;
-    cout << "Maximum value " << result.maximum << nl;
-    cout << nl;
-
-    CRACKED;
+    cout << "Minimum value: " << result.minimum << "\n";
+    cout << "Maximum value: " << result.maximum << "\n";
+    return 0;
 }
